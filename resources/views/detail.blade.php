6ef896aca8d259990a1b35abf790494c6cc7e7d7
Gerry Guinardi <73393827+rousin1408@users.noreply.github.com>
2022-11-27 18:34:32 +0700

    final

--- resources/views/detail.blade.php
+++ resources/views/detail.blade.php
@@ -22,30 +22,4 @@
             </div>
         </div>
     </div>
-
-
-    {{-- <div>
-        <div class="index">
-            <h1>Book List</h1>
-        </div>
-        <div class="row">
-            @foreach ($books as $book)
-                <div class="col-lg-3 col-md-4">
-                    <div class="card">
-                        <img src="{{ \Illuminate\Support\Facades\URL::asset('image/' . $book->image) }}" class="card-img-top"
-                            alt="Book">
-                        <div class="card-body">
-                            <h6 class="card-title">{{ $book->title }}</h6>
-                            <p class="card-text author">By <span>{{ $book->author }}</span></p>
-                            <a href="/book/{{ $book->id }}" class="btn btn-warning">Book Detail</a>
-                        </div>
-                    </div>
-                </div>
-            @endforeach
-        </div> --}}
-    {{-- </div> --}}
-
-
-
-
 @endsection
